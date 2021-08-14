07717735e11c14201dadadf5146dbf5d95343129
KAESEAK <88919059+KAESEAK@users.noreply.github.com>
2021-08-14 18:38:26 +0800

    Update surf_notify.py

--- surf_notify.py
+++ surf_notify.py
@@ -50,7 +50,7 @@ for map in maps:
 if maprunning == False:
     timestamp = datetime.today()
     strtimestamp = timestamp.strftime("%d/%m/%Y %H:%M:%S")
-    print("No maps of interest currently. --- " + strtimestamp)
+    print("No more maps of interest currently running. --- " + strtimestamp)
 
 # Check maps every x seconds
 x = 900
@@ -70,5 +70,4 @@ while True:
     if maprunning == False:
         timestamp = datetime.today()
         strtimestamp = timestamp.strftime("%d/%m/%Y %H:%M:%S")
-        print("No maps of interest currently running. --- " + strtimestamp)
-
+        print("No more maps of interest currently running. --- " + strtimestamp)
